16187d3021c314f09b9e7d66ad0e1324c8190f97
wangqingye <329103586@qq.com>
2017-05-18 23:30:25 +0800

    基础显示类

--- src/GameSence/BaseSence.ts
+++ src/GameSence/BaseSence.ts
@@ -8,7 +8,36 @@ class BaseScene extends egret.DisplayObjectContainer
     {
         super();
         this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
-        
+        StageUtils.stage.addEventListener(egret.Event.RESIZE, this.onResize, this);
+    }
+
+    private onAddToStage(event:egret.Event)
+    {
+        this.init();
+    }
+
+    /**初始化*/
+    protected init()
+    {
+        TimerManager.doFrame(1, 0, this.update, this);
+    }
+
+    /**帧更新*/
+    protected update(time:number)
+    {
+
+    }
+
+    protected onResize()
+    {
+
+    }
+
+    /**退出释放*/
+    public dispos()
+    {
+        TimerManeger.remove(this.update, this);
+        DisplayUitls.removeFromParent(this);
     }
 }
 
