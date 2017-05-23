be7b2368840dcee5e2d25ba92bdc6839fad19333
wangqingye <329103586@qq.com>
2017-05-23 23:41:49 +0800

    文本

--- src/GameItem/Label.ts
+++ src/GameItem/Label.ts
@@ -1,1 +1,22 @@
-// TypeScript file
+/**
+ * 文本 
+*/
+
+class Label extends egret.TextField
+{
+    public constructor()
+    {
+        super();
+        this.fontFamily = "Cookies";
+        this.textAlign = "center";
+        AnchorUtils.setAnchor(this, 0.5);
+    }
+
+    public $setText(value:string): boolean
+    {
+        super.$setText(value);
+        this.anchorOffsetX = this["anchorX"] * this.width;
+        this.anchorOffsetY = this["anchorY"] * this.height;
+        return true;
+    }
+}
--- src/GameSence/LoadingSence.ts
+++ src/GameSence/LoadingSence.ts
@@ -1,1 +1,26 @@
-// TypeScript file
+/**
+ * 加载界面
+*/
+
+class LoadingSence extends BaseScene
+{
+    private _bg: egret.Bitmap;
+    private _loadingFrame: egret.Bitmap;
+    private _loadingBg: egret.Bitmap;
+    private _loadingBar: egret.Bitmap;
+    private _label:egret.TextField;
+
+    protected init()
+    {
+        super.init();
+
+        this.addChild(this._bg = DisplayuUtils.createBitmap("bg_png"));
+        this.addChild(this._loadingBg = DisplayuUtils.createBitmap("loading3_png"));
+        this.addChild(this._loadingBar = DisplayuUtils.createBitmap("loading2_png"));
+        this._loadingBar.width = 0;
+        this.addChild(this._loadingFrame = DisplayuUtils.createBitmap("loading1_png"));
+
+        this.addChild(this._label = new Label)
+    }
+
+}
